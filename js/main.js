40e84c9ef3ef8504c763c07c266fbd50f93f6032
trosendo <trosendo@users.noreply.github.com>
2018-05-21 14:58:51 +0100

    Better loadNext for books to show up

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var book1 = {
     img: "resources/img1.jpg",
     author: "Patrick Rothfuss",
     description: "Da infância como membro de uma família unida de nómadas Edema Ruh até à provação dos primeiros dias como aluno de magia numa universidade prestigiada, o humilde estalajadeiro Kvothe relata a história de como um rapaz desfavorecido pelo destino se torna um herói, um bardo, um mago e uma lenda. O primeiro romance de Rothfuss lança uma trilogia relatando não apenas a história da Humanidade, mas também a história de um mundo ameaçado por um mal cuja existência nega de forma desesperada. O autor explora o desenvolvimento de uma personalidade enquanto examina a relação entre a lenda e a sua verdade, a verdade que reside no coração das histórias. Contada de forma elegante e enriquecida com vislumbres de histórias futuras, esta \"autobiografia\" de um herói rica em detalhes é altamente recomendada para bibliotecas de qualquer tamanho.",
+    opinion: "",
     links: [
         {
             url: "https://pt.wikipedia.org/wiki/O_Nome_do_Vento", 
@@ -27,6 +28,7 @@ var book2 = {
     img: "resources/img2.jpg",
     author: "Cormac McCarthy",
     description: "A father and his son walk alone through burned America. Nothing moves in the ravaged landscape save the ash on the wind. It is cold enough to crack stones, and when the snow falls it is gray. The sky is dark. Their destination is the coast, although they don’t know what, if anything, awaits them there. They have nothing; just a pistol to defend themselves against the lawless bands that stalk the road, the clothes they are wearing, a cart of scavenged food—and each other.",
+    opinion: "",
     links: [
         {
             url: "https://pt.wikipedia.org/wiki/A_Estrada_(livro)", 
@@ -51,6 +53,7 @@ var book3 = {
     img: "resources/img3.jpg",
     author: "Patrick Ness",
     description: "Adam Thorn is having what will turn out to be the most unsettling, difficult day of his life, with relationships fracturing, a harrowing incident at work, and a showdown between this gay teen and his preacher father that changes everything. It's a day of confrontation, running, sex, love, heartbreak, and maybe, just maybe, hope. He won't come out of it unchanged. And all the while, lurking at the edges of the story, something extraordinary and unsettling is on a collision course.",
+    opinion: "",
     links: [
         {
             url: "https://pt.wikipedia.org/wiki/Patrick_Ness", 
@@ -91,11 +94,14 @@ class Library{
         }
         this.seenBooks.push(this.books.splice(0, 1)[0]);
     }
-    loadNext(){
+    loadNext(opinion){
         // var i = this.nextIndex();
         // console.log(i);
         if(this.books[0] == undefined){
-            return undefined;
+            return -1;
+        }
+        if(opinion != undefined){
+            this.books[0].opinion = opinion;
         }
         this.loadBook(this.books[0]);
         return 0;
@@ -105,6 +111,9 @@ class Library{
             this.books.push(this.seenBooks[i]);
         }
         this.seenBooks = [];
+        this.books.forEach(function(value, index){
+            console.log(value);
+        });
     }
     addBook(book){
         this.books.push(book);
@@ -133,7 +142,7 @@ $("button.like-dislike").click(function () {
         $("label#dislikes").html(dislikes);
     }
     // console.log(opinions[book_count++]);
-    if(library.loadNext() == undefined){
+    if(library.loadNext(opinion) == -1){
         $(".book").addClass("inactive");
         $(".stats").removeClass("inactive");
     }
@@ -160,4 +169,3 @@ $("button.reset").click(function() {
     
 //     $next.addClass("active");
 // });
-
